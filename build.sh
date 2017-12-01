53b2c5ad095da938a7608103e40543c70d0eb5fe
superafroman <max@cautionyourblast.com>
2017-12-01 15:43:54 +0000

    Initial configuration of learning locker components for Docker.

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+PREFIX=learning-locker-
+ORGANISATION="cautionyourblast"
+IMAGES=( "base" "api-server" "api-worker" "ui" )
+
+function build {
+    IMAGE=$1
+    echo "Building ${ORGANISATION}/${PREFIX}${IMAGE}"
+    docker build -t ${ORGANISATION}/${PREFIX}${IMAGE} ${IMAGE}/.
+}
+
+for image in "${IMAGES[@]}"
+do
+    build ${image}
+done
+
+echo "All builds complete"
